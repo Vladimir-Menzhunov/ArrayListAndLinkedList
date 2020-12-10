94bb0776c5d0cdd4f7e71a92575bb1b5cd718ab1
Vladimir <vi.vova2000@gmail.com>
2020-12-10 16:28:52 +0300

    1.0.1

--- src/test/java/TestArrayListAndLinkedList.java
+++ src/test/java/TestArrayListAndLinkedList.java
@@ -1,13 +1,11 @@
+import javafx.beans.binding.When;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.*;
 
 public class TestArrayListAndLinkedList {
-
+    List<List<Double>> list = getList(20000000);
     /**
      * get(int index) - (LinkedList) - Время выполнения операции линейно O(n) (во всех случаях кроме
      * добавления в начало или конец там оно константно O(1)
@@ -16,62 +14,33 @@ public class TestArrayListAndLinkedList {
      */
 
     @Test
-    public void checkGetMethodArrayListAndLinkedList() {
-        long startTime;
-        long finishTime;
-
-        long timeArrayList;
+    public void checkGetMethodArrayListAndLinkedList()  {
+        Stack<Long> timesList;
         long timeLinkedList;
+        long timeArrayList;
 
-        ArrayList<Double> arrayList = new ArrayList<Double>();
-        LinkedList<Double> linkedList = new LinkedList<Double>();
 
-        int range = 100000000;
-
-        for(int i = 0; i < 20000000; i++) {
-            arrayList.add(Math.random() * range);
-            linkedList.add(Math.random() * range);
-        }
-
-        startTime = System.currentTimeMillis();
-        arrayList.get(10000000);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-        linkedList.get(10000000);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, list.get(0).size() / 2, "get");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList get middle = " + timeArrayList);
         System.out.println("TimeLinkedList get middle = " + timeLinkedList);
-
         assertTrue(timeArrayList < timeLinkedList);
 
-        startTime = System.currentTimeMillis();
-        arrayList.get(0);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-        linkedList.getFirst();
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, 0, "get");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList get first = " + timeArrayList);
         System.out.println("TimeLinkedList get first = " + timeLinkedList);
 
         assertEquals(timeArrayList, timeLinkedList);
 
-        startTime = System.currentTimeMillis();
-        arrayList.get(arrayList.size() - 1);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, list.get(0).size() - 1, "get");
 
-        startTime = System.currentTimeMillis();
-        linkedList.getLast();
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList get last = " + timeArrayList);
         System.out.println("TimeLinkedList get last = " + timeLinkedList);
@@ -79,6 +48,7 @@ public class TestArrayListAndLinkedList {
         assertEquals(timeArrayList, timeLinkedList);
 
     }
+
 
     /**
      * add(int index) - (LinkedList) - Время выполнения операции линейно O(n) (во всех случаях кроме
@@ -90,61 +60,31 @@ public class TestArrayListAndLinkedList {
 
     @Test
     public void checkAddMethodArrayListAndLinkedList() {
-        long startTime;
-        long finishTime;
-
-        long timeArrayList;
+        Stack<Long> timesList;
         long timeLinkedList;
+        long timeArrayList;
 
-        ArrayList<Double> arrayList = new ArrayList<Double>();
-        LinkedList<Double> linkedList = new LinkedList<Double>();
-
-        int range = 10000000;
-
-        for(int i = 0; i < 20000000; i++) {
-            arrayList.add(Math.random() * range);
-            linkedList.add(Math.random() * range);
-        }
-
-        startTime = System.currentTimeMillis();
-        arrayList.add(10000000, 5000.);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-        linkedList.add(10000000, 5000.);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, list.get(0).size() / 2, "add");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList add middle = " + timeArrayList);
         System.out.println("TimeLinkedList add middle = " + timeLinkedList);
-
         assertTrue(timeArrayList < timeLinkedList);
 
-        startTime = System.currentTimeMillis();
-        arrayList.add(0, 5000.);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-        linkedList.add(0, 5000.);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, 0, "add");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList add first = " + timeArrayList);
         System.out.println("TimeLinkedList add first = " + timeLinkedList);
 
         assertTrue(timeArrayList > timeLinkedList);
 
-        startTime = System.currentTimeMillis();
-        arrayList.add(arrayList.size() - 1, 5000.);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, list.get(0).size() - 1, "add");
 
-        startTime = System.currentTimeMillis();
-        linkedList.add(linkedList.size() - 1, 5000.);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList add last = " + timeArrayList);
         System.out.println("TimeLinkedList add last = " + timeLinkedList);
@@ -164,61 +104,31 @@ public class TestArrayListAndLinkedList {
 
     @Test
     public void checkRemoveMethodArrayListAndLinkedList() {
-        long startTime;
-        long finishTime;
-
-        long timeArrayList;
+        Stack<Long> timesList;
         long timeLinkedList;
+        long timeArrayList;
 
-        ArrayList<Double> arrayList = new ArrayList<Double>();
-        LinkedList<Double> linkedList = new LinkedList<Double>();
-
-        int range = 10000000;
-
-        for(int i = 0; i < 20000000; i++) {
-            arrayList.add(Math.random() * range);
-            linkedList.add(Math.random() * range);
-        }
-
-        startTime = System.currentTimeMillis();
-        arrayList.remove(10000000);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-        linkedList.remove(10000000);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, list.get(0).size() / 2, "remove");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList remove middle = " + timeArrayList);
         System.out.println("TimeLinkedList remove middle = " + timeLinkedList);
-
         assertTrue(timeArrayList < timeLinkedList);
 
-        startTime = System.currentTimeMillis();
-        arrayList.remove(0);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-        linkedList.remove(0);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, 0, "remove");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList remove first = " + timeArrayList);
         System.out.println("TimeLinkedList remove first = " + timeLinkedList);
 
         assertTrue(timeArrayList > timeLinkedList);
 
-        startTime = System.currentTimeMillis();
-        arrayList.remove(arrayList.size() - 1);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
+        timesList = leadTimeGetMethod(list, list.get(0).size() - 1, "remove");
 
-        startTime = System.currentTimeMillis();
-        linkedList.remove(linkedList.size() - 1);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
         System.out.println("TimeArrayList remove last = " + timeArrayList);
         System.out.println("TimeLinkedList remove last = " + timeLinkedList);
@@ -236,47 +146,18 @@ public class TestArrayListAndLinkedList {
 
     @Test
     public void checkIteratorRemoveMethodArrayListAndLinkedList() {
-        long startTime;
-        long finishTime;
-
-        long timeArrayList;
+        Stack<Long> timesList;
         long timeLinkedList;
+        long timeArrayList;
 
-        ArrayList<Double> arrayList = new ArrayList<Double>();
-        LinkedList<Double> linkedList = new LinkedList<Double>();
+        List<List<Double>> listForRemove = getList(200000);
 
-        int range = 10000000;
+        timesList = leadTimeGetMethod(listForRemove, -1, "iteratorRemove");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
 
-        for(int i = 0; i < 200000; i++) {
-            arrayList.add(Math.random() * range);
-            linkedList.add(Math.random() * range);
-        }
-
-        startTime = System.currentTimeMillis();
-
-        Iterator<Double> iteratorArrayList = arrayList.iterator();
-        while (iteratorArrayList.hasNext()) {
-            iteratorArrayList.next();
-            iteratorArrayList.remove();
-        }
-
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-
-        Iterator<Double> iteratorLinkedList = linkedList.iterator();
-        while (iteratorLinkedList.hasNext()) {
-            iteratorLinkedList.next();
-            iteratorLinkedList.remove();
-        }
-
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
-
-        System.out.println("TimeArrayList iteratorArrayList remove = " + timeArrayList);
-        System.out.println("TimeLinkedList iteratorLinkedList remove = " + timeLinkedList);
-
+        System.out.println("TimeArrayList iteratorRemove = " + timeArrayList);
+        System.out.println("TimeLinkedList iteratorRemove = " + timeLinkedList);
         assertTrue(timeArrayList > timeLinkedList);
 
     }
@@ -290,37 +171,62 @@ public class TestArrayListAndLinkedList {
 
     @Test
     public void checkListIteratorAddMethodArrayListAndLinkedList() {
+        Stack<Long> timesList;
+        long timeLinkedList;
+        long timeArrayList;
+
+        timesList = leadTimeGetMethod(list, -1, "iteratorAdd");
+        timeLinkedList = timesList.pop();
+        timeArrayList = timesList.pop();
+
+        System.out.println("TimeArrayList iteratorAdd = " + timeArrayList);
+        System.out.println("TimeLinkedList iteratorAdd = " + timeLinkedList);
+        assertTrue(timeArrayList > timeLinkedList);
+    }
+
+
+    public List<List<Double>> getList(int range) {
+        List<List<Double>> list = new ArrayList<List<Double>>();
+        list.add(new ArrayList<Double>());
+        list.add(new LinkedList<Double>());
+        for (List<Double> doubles : list) {
+            for (int index = 0; index < range; index++) {
+                doubles.add(Math.random() * range);
+            }
+        }
+
+        return list;
+    }
+
+    public Stack<Long> leadTimeGetMethod(List<List<Double>> list, int index, String method){
         long startTime;
         long finishTime;
 
-        long timeArrayList;
-        long timeLinkedList;
+        Stack<Long> timesLists = new Stack<Long>();
+        for (List<Double> doubles : list) {
+            startTime = System.currentTimeMillis();
 
-        ArrayList<Double> arrayList = new ArrayList<Double>();
-        LinkedList<Double> linkedList = new LinkedList<Double>();
+            if(method.contains("get")) doubles.get(index);
+            if(method.contains("add")) doubles.add(index, 5000.);
+            if(method.contains("remove")) doubles.remove(index);
+            if(method.contains("iteratorRemove")) {
+                Iterator<Double> iteratorList = doubles.iterator();
+                while (iteratorList.hasNext()) {
+                    iteratorList.next();
+                    iteratorList.remove();
+                }
+            }
+            if(method.contains("iteratorAdd")) {
+                ListIterator<Double> doubleListIteratorList = doubles.listIterator();
+                doubleListIteratorList.add(5000.);
+            }
 
-        int range = 10000000;
-
-        for (int i = 0; i < 20000000; i++) {
-            arrayList.add(Math.random() * range);
-            linkedList.add(Math.random() * range);
+            finishTime = System.currentTimeMillis();
+            timesLists.add(finishTime - startTime);
         }
 
-        startTime = System.currentTimeMillis();
-        ListIterator<Double> doubleListIteratorArrayList = arrayList.listIterator();
-        doubleListIteratorArrayList.add(5000.);
-        finishTime = System.currentTimeMillis();
-        timeArrayList = finishTime - startTime;
-
-        startTime = System.currentTimeMillis();
-        ListIterator<Double> doubleListIteratorLinkedList = linkedList.listIterator();
-        doubleListIteratorLinkedList.add(5000.);
-        finishTime = System.currentTimeMillis();
-        timeLinkedList = finishTime - startTime;
-
-        System.out.println("TimeArrayList ListIteratorAdd  = " + timeArrayList);
-        System.out.println("TimeLinkedList ListIteratorAdd = " + timeLinkedList);
-
-        assertTrue(timeArrayList > timeLinkedList);
+        return timesLists;
     }
+
+
 }
